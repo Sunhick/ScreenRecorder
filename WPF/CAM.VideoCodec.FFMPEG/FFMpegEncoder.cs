294011577532e8212e701827022d86f18b886f57
sunhick <sunhick@gmail.com>
2015-06-25 00:18:01 +0530

    Redirect standard out and error of FFMPEG process to Log4Net

--- WPF/CAM.VideoCodec.FFMPEG/FFMpegEncoder.cs
+++ WPF/CAM.VideoCodec.FFMPEG/FFMpegEncoder.cs
@@ -15,6 +15,8 @@
 
 using System.Diagnostics;
 using System.IO;
+using System.Text;
+using System.Threading;
 using CAM.Common;
 using CAM.VideoCodec.Interfaces;
 using log4net;
@@ -27,54 +29,96 @@ namespace CAM.VideoCodec.FFMPEG
     // ReSharper disable once InconsistentNaming
     public class FFMpegEncoder : IFFMpegEncoder
     {
-        private readonly ILog Log = LogManager.GetLogger(typeof (FFMpegEncoder));
+        private readonly ILog Log = LogManager.GetLogger(typeof(FFMpegEncoder));
 
         public string Encode(EncoderInfo theEncoderInfo)
         {
             Log.Info("Start of encoding bitmaps into video stream...");
-            var aVMaker = new Process
-            {
-                StartInfo =
-                {
-                    FileName = Path.Combine(theEncoderInfo.ExePath, theEncoderInfo.ExeName)
-                    //RedirectStandardOutput = true,
-                    //RedirectStandardError = true
-                }
-            }; //Audio-Video maker process
 
-            //TODO: ask If output.mp4 need to be overwritten?
+            //Audio-Video maker process
+            using (var aVMaker = new Process())
+            {
+                aVMaker.StartInfo.FileName = Path.Combine(theEncoderInfo.ExePath, theEncoderInfo.ExeName);
+                aVMaker.StartInfo.RedirectStandardOutput = true;
+                aVMaker.StartInfo.RedirectStandardError = true;
 
 #if DEBUG
-            aVMaker.StartInfo.UseShellExecute = false;
-            aVMaker.StartInfo.CreateNoWindow = false;
+                aVMaker.StartInfo.UseShellExecute = false;
+                aVMaker.StartInfo.CreateNoWindow = false;
 #else 
-            aVMaker.StartInfo.UseShellExecute = false;
-            aVMaker.StartInfo.CreateNoWindow = true;
+                aVMaker.StartInfo.UseShellExecute = false;
+                aVMaker.StartInfo.CreateNoWindow = true;
 #endif
-            var aOutFile = GetOutputFile(theEncoderInfo.HookId);
+                var aOutFile = GetOutputFile(theEncoderInfo.HookId);
 
-            //avMaker.StartInfo.Arguments = String.Format(@"-i bitmaps\{0} -vcodec huffyuv output.avi", pngLoc);
-            //avMaker.StartInfo.Arguments = String.Format(@"-i bitmaps\{0} -r 20 output.mp4", pngLoc);
+                //avMaker.StartInfo.Arguments = String.Format(@"-i bitmaps\{0} -vcodec huffyuv output.avi", pngLoc);
+                //avMaker.StartInfo.Arguments = String.Format(@"-i bitmaps\{0} -r 20 output.mp4", pngLoc);
 
-            aVMaker.StartInfo.Arguments = theEncoderInfo.Arguments + "\\" + aOutFile;
-            // String.Format(@"-i {0} -r {2} -c:v libx264 -preset slow -crf 21 {1}", ifile, outFile, Settings.Default.FramesPerSec);
-            // avMaker.StartInfo.Arguments = String.Format(@" -r 20 -i bitmaps\{0} -c:v libx264 -r 20 -pix_fmt yuv420p output.mp4", pngLoc);
+                aVMaker.StartInfo.Arguments = theEncoderInfo.Arguments + "\\" + aOutFile;
+                // String.Format(@"-i {0} -r {2} -c:v libx264 -preset slow -crf 21 {1}", ifile, outFile, Settings.Default.FramesPerSec);
+                // avMaker.StartInfo.Arguments = String.Format(@" -r 20 -i bitmaps\{0} -c:v libx264 -r 20 -pix_fmt yuv420p output.mp4", pngLoc);
 
-            if (!aVMaker.Start())
-            {
-                return null;
+                var aOutput = new StringBuilder();
+                var aError = new StringBuilder();
+
+                using (var aOutputWaitHandle = new AutoResetEvent(false))
+                {
+                    using (var aErrorWaitHandle = new AutoResetEvent(false))
+                    {
+                        {
+                            aVMaker.OutputDataReceived += (theSender, theArgs) =>
+                            {
+                                if (theArgs.Data == null)
+                                {
+                                    aOutputWaitHandle.Set();
+                                }
+                                else
+                                {
+                                    aOutput.AppendLine(theArgs.Data);
+                                }
+                            };
+                            aVMaker.ErrorDataReceived += (theSender, theArgs) =>
+                            {
+                                if (theArgs.Data == null)
+                                {
+                                    aErrorWaitHandle.Set();
+                                }
+                                else
+                                {
+                                    aError.AppendLine(theArgs.Data);
+                                }
+                            };
+
+                            if (!aVMaker.Start())
+                            {
+                                Log.Error("Error in starting the FFMPEG process!");
+                            }
+
+                            aVMaker.BeginOutputReadLine();
+                            aVMaker.BeginErrorReadLine();
+
+                            // Log FFMPEG output and errors
+                            Log.Info(aOutput);
+                            Log.Error(aError);
+
+                            if (aVMaker.WaitForExit(int.MaxValue) &&
+                                aOutputWaitHandle.WaitOne() &&
+                                aErrorWaitHandle.WaitOne())
+                            {
+                                // Process completed. Check process.ExitCode here.
+                                Log.Info("FFMPEG Process completed! Error code:" + aVMaker.ExitCode);
+                            }
+                            else
+                            {
+                                // Timed out.
+                                Log.Info("FFMPEG Process timeout!");
+                            }
+
+                            return aOutFile;
+                        }
+                    }
+                }
             }
-
-            aVMaker.WaitForExit();
-            aVMaker.Close();
-
-            //// output
-            // Log.Info(aVMaker.StandardOutput.ReadToEnd());
-            ////errors
-            //Log.Error(aVMaker.StandardError.ReadToEnd());
-
-            Log.Info("End of encoding bitmaps into video stream...");
-            return aOutFile;
         }
 
         private string GetOutputFile(string theHookId)
